cc22bc02ed3ddff69a1d077c1a21bb13e6a46b26
aycetheeeng <102387422+aycetheeeng@users.noreply.github.com>
2026-04-20 03:08:45 +0300

    Initialize SerialHandler in main.cpp
    Added SerialHandler instance and context property to QML engine.

---
+++ main.cpp
@@ -0,0 +1,36 @@
+// Copyright (C) 2024 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+
+#include <QApplication>
+#include <QQmlApplicationEngine>
+#include "serialhandler.h"
+#include <QQmlContext>
+#include "autogen/environment.h"
+
+SerialHandler serialHandler; // ben ekledim
+
+int main(int argc, char *argv[])
+{
+    set_qt_environment();
+    QApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.rootContext()->setContextProperty("serialHandler", &serialHandler); // ben ekledim
+    const QUrl url(mainQmlFile);
+    QObject::connect(
+                &engine, &QQmlApplicationEngine::objectCreated, &app,
+                [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+
+    engine.addImportPath(QCoreApplication::applicationDirPath() + "/qml");
+    engine.addImportPath(":/");
+
+    engine.load(url);
+
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
+}
